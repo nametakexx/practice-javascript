9af915dfe19cfc008e29c5f9a44eb4b29a428ccc
nametakexx <narukikan@gmail.com>
2024-09-20 04:50:20 +0000

    9_19の分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,19 @@
+document.querySelector("h1").textContent="神戸"
+
+//documentとは!!
+/*htmlファイルで書かれているコードを一つのオブジェクトとみなす。*/
+
+//DOM（ドム）
+/*ドキュメントオブジェクトモデル(Document object model)の略で
+Webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//上記コードの意味
+//document.querySelector("h1").textContent="自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.tectcontent="自分の名前"
+
+document.querySelector("h2").textContent="神戸"
