9b2a0828587252c144856fad7de6691083adca23
nametakexx <narukikan@gmail.com>
2024-11-22 06:20:51 +0000

    画像を適切に表示

--- RPS.js
+++ RPS.js
@@ -41,10 +41,10 @@ function RPS(){
     image.style.height = "200px";
     image.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiusq_ptNOOC9XkVvWTa88nhB6I7n12fsf95zdnS-n269HpN9dVRd0JCCV0iL2u_tWrDU5XySM8-i9u38-tXp0Wgu6qRF4p-5A1djjVskwkB0SQFxULDss8Uj1o7CYfbMNRpT-kfn3cG4E/s800/janken_gu.png";
 
-    let enemyImage = document.createElement("image");
+    let enemyImage = document.createElement("img");
     enemyImage.alt="Choice";
-    image.style.width = "200px";
-    image.style.height = "200px";
+    enemyImage.style.width = "200px";
+    enemyImage.style.height = "200px";
 
     choiceButton.addEventListener("click",function(){
         switch(select.value){
@@ -60,11 +60,26 @@ function RPS(){
             default:
                 break;
         }
-        let enemyHand = choices[Math.floor(Math.random() * choices.length)]
-        console.log(enemyhand);
+        let enemyHand = choices[Math.floor(Math.random() * choices.length)];
         
+        switch(enemyHand){
+            case "gu":
+                enemyImage.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiusq_ptNOOC9XkVvWTa88nhB6I7n12fsf95zdnS-n269HpN9dVRd0JCCV0iL2u_tWrDU5XySM8-i9u38-tXp0Wgu6qRF4p-5A1djjVskwkB0SQFxULDss8Uj1o7CYfbMNRpT-kfn3cG4E/s800/janken_gu.png";
+                break;
+            case "choki":
+                enemyImage.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhSYYvy3_ZU5FYb8Jug1Gssh483SEIn8hSwWO33rp-7j9m5AFsn9Fyis9oT1DKvykpCEMV6bJGMAaTtABep-1qqr9ZPtiI_aQQsJVWNL6H_i-b6I3O_1-dgwmavPoEI9HHMsuHHPQCHj90/s800/janken_choki.png";
+                break;
+             case "pa":
+                enemyImage.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQhsfUvWhhVJej7FEqYsQbe0EwLCOHYxKU4KnrF026nnfJkiM3yQO2NFmnnX0nD4P2IdCmg8qFQpZMW8vtbs-K7sLpoCqXwO0fkTT7UL5VkM-E2MOUNXpikYfspDKaxidAehqcuQoIrcM/s800/janken_pa.png";
+                break;
+            default:
+                break;
+        }
+
     })
     gameCountainer.appendChild(image);
+    gameCountainer.appendChild(enemyImage);
+
 
 
 
